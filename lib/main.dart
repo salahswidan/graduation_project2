8bf3a70b48a56c8551bc273b6a44dcacb851f435
salah <salahswidan212@gmail.com>
2024-06-13 19:09:04 +0300

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OptiMen(),
+      home: Splash(),
     );
   }
 }
